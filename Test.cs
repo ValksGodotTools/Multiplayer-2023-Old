29f0f8aabe141cf24d6307906295ab30252c2967
valkyrienyanko <sebastianbelle074@protonmail.com>
2023-03-12 20:22:45 -0500

    first commit

---
+++ Test.cs
@@ -0,0 +1,17 @@
+global using Godot;
+global using GodotUtils;
+global using System;
+
+using GodotUtils.UI;
+
+namespace Sandbox2;
+
+public partial class Test : UIConsole
+{
+	public override void _Ready()
+	{
+		base._Ready();
+
+		UIConsole.Test();
+	}
+}
