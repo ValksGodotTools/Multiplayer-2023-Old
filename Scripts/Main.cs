74d0f2ff433e09d2cea23f54cf2d9b752a2858e5
valkyrienyanko <sebastianbelle074@protonmail.com>
2023-03-13 21:44:37 -0500

    update

--- Scripts/Main.cs
+++ Scripts/Main.cs
@@ -8,7 +8,14 @@ public partial class Main : Node
 {
 	public override void _Ready()
 	{
-		
+		try 
+		{
+			var arr = new int[2];
+			arr[2] = 1;
+		} catch (Exception e)
+		{
+			Logger.Log(e, ConsoleColor.Green);
+		}
 	}
 
 	public override void _PhysicsProcess(double delta)
