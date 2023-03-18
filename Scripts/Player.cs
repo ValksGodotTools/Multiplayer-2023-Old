b652bae8510232822de3bc893ecbccb4e17ef9fb
valkyrienyanko <sebastianbelle074@protonmail.com>
2023-03-18 02:06:51 -0500

    Prevent player from moving while console is open

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -4,5 +4,9 @@ using GodotUtils.TopDown;
 
 public partial class Player : PlayerController
 {
-
+    public override void _PhysicsProcess(double delta)
+    {
+        if (!UIConsole.IsVisible)
+            base._PhysicsProcess(delta);
+    }
 }
